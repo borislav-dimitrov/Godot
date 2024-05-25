7b1e4a4d7a35403d2998a6230743c51adcd93681
Borislav Dimitrov <borislav-dimitrov@outlook.com>
2024-05-25 21:37:32 +0300

    place cards on field checkpoint

--- TCG/scripts/battle_field/battle_field_cell.gd
+++ TCG/scripts/battle_field/battle_field_cell.gd
@@ -1,20 +1,26 @@
 class_name BattleFieldCell
-extends ColorRect
+extends Container
 
 @onready var anim: AnimationPlayer = $Anim
+@onready var bg: Sprite2D = $Background
+@onready var content: Sprite2D = $Sprite2D
+
+@export var can_spawn: bool = false
 
 var mouse_over: bool = false
+var is_empty: bool = true
 
 func _handle_click_down():
-	print(name)
+	if can_spawn:
+		content.show()
 
 func _on_mouse_entered():
 	mouse_over = true
-	anim.play('mouse_enter')
+	anim.play('mouse_in')
 
 func _on_mouse_exited():
 	mouse_over = false
-	anim.play('mouse_exit')
+	anim.play('mouse_out')
 	
 
 func _on_gui_input(event):
@@ -23,4 +29,3 @@ func _on_gui_input(event):
 			if event.pressed:
 				# Grab/click
 				_handle_click_down()
-
